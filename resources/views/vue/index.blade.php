237d23df5aa866d59ca013fb001524c7697388fd
vagrant <vagrant@homestead>
2019-03-22 17:58:46 +0000

    Get number of switches functional with button

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -1,39 +1,41 @@
 @extends('master')
 
-@section('title', 'Vue.js App')
+@section('title', 'Manager OpenFlow')
 
 @section('content')
 
-<div class="jumbotron">
-    <h1></h1>
-</div>
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Age</th>
-            <th>Department</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
-
-<div class=" alert alert-success">
-    <button type="button" class="close-btn" >&times;</button>
-    <strong></strong>
-</div>
-
-<div class="jumbotron">
-    <h2>Edit User</h2>
-
+<div class="jumbotron" id="">
+    <button v-on:click="getNumberOfSwitches">GET</button>
+    <p>@{{switchesCount}}</p>
 </div>
 
 
 @endsection
 @section('pagescript')
-<script src="js/vue.js"></script>
+<script src="http://unpkg.com/vue"></script>
+<script src="http://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+    var vm = new Vue ({
+        el: '#app',
+        data: {
+            switchesInfo: [],
+            switchesCount: 0,
+        },
+        methods: {
+            getNumberOfSwitches: function () {
+                this.switchesInfo = [];
+                var vm = this;
+                axios.get('http://10.10.10.2:8181/restconf/operational/opendaylight-inventory:nodes', 
+                            { headers: { "Authorization": "Basic YWRtaW46YWRtaW4="}})
+                    .then(function (response){
+                        vm.switchesInfo = response.data;
+                        vm.switchesCount = vm.switchesInfo.nodes.node.length;
+                    })
+                    .catch(function (error){
+                        vm.switchesInfo = 'An error occurred.' + error;
+                    });
+            }
+        },
+    });
+</script>
 @stop  
