89b85cd54a577c13890fa58f6950ec0f23d5c4c7
joelferreira14 <48366483+joelferreira14@users.noreply.github.com>
2019-03-26 17:20:55 +0000

    pagina login
    pagina login

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -5,8 +5,19 @@
 @section('content')
 
 <div class="jumbotron" id="">
-    <button v-on:click="getNumberOfSwitches">GET</button>
-    <p>@{{switchesCount}}</p>
+    <p>Insert ip address</p>
+    <input v-model="ipAndPort" placeholder="<ip address>:<port>">
+    <br> <br>
+    <p>Login: </p>
+    <input v-model="username" placeholder="username">
+    <input type="password" v-model="password" placeholder="password">
+    <button v-on:click= "getNumberOfSwitches(ipAndPort, username, password)">SEND</button><br><br>
+
+    <p>string: @{{str}}</p>
+    <p>base64: @{{encodedStr}}</p>
+
+
+
 </div>
 
 
@@ -14,19 +25,31 @@
 @section('pagescript')
 <script src="http://unpkg.com/vue"></script>
 <script src="http://unpkg.com/axios/dist/axios.min.js"></script>
+
+
+
 <script>
     var vm = new Vue ({
         el: '#app',
         data: {
             switchesInfo: [],
             switchesCount: 0,
+            str:'',
+            encodedStr:'',
         },
+    
         methods: {
-            getNumberOfSwitches: function () {
+            getNumberOfSwitches: function (ipAndPort, username, password) {
                 this.switchesInfo = [];
+                this.str = `${username}:${password}`;
+                //this.encodedStr = btoa(this.str);
+                this.encodedStr = btoa(this.str);
+
                 var vm = this;
-                axios.get('http://10.10.10.2:8181/restconf/operational/opendaylight-inventory:nodes', 
-                            { headers: { "Authorization": "Basic YWRtaW46YWRtaW4="}})
+                //axios.get('http://10.10.10.2:8181/restconf/operational/opendaylight-inventory:nodes', 
+                axios.get(`http://${ipAndPort}/restconf/operational/opendaylight-inventory:nodes`, 
+                            //{ headers: { "Authorization": "Basic YWRtaW46YWRtaW4="}}) admin:admin
+                            { headers: { "Authorization": 'Basic @{{encodedStr}'}})
                     .then(function (response){
                         vm.switchesInfo = response.data;
                         vm.switchesCount = vm.switchesInfo.nodes.node.length;
@@ -38,4 +61,6 @@
         },
     });
 </script>
+
+
 @stop  
