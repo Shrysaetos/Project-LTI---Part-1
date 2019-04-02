2a505a83005cfdf2ba276934471b029f20d3fc58
joelferreira14 <48366483+joelferreira14@users.noreply.github.com>
2019-04-02 23:35:43 +0100

    Add files via upload

--- resources/js/components/CreateFlow.vue
+++ resources/js/components/CreateFlow.vue
@@ -2,17 +2,58 @@
   <div>
     <div class="jumbotron">
       <h1>Create Flow</h1>
-    </div>
+      <p>TESTE URL: {{device}} | {{flowId}} | {{flowTable}} | {{priority}}</p>
 
-    <div class="alert" :class="{'alert-success':showSuccess,'alert-danger':showFailure}" v-if="showSuccess || showFailure">
-      <strong>{{successMessage}}</strong>
-      <strong>{{failMessage}}</strong>
     </div>
 
     <div>
-        
+      <label>Device: </label>
+      <select v-model="device">
+        <option disabled value="">Please select one</option>
+        <option v-for="s in switchesInfo.nodes.node">{{s.id}}</option>
+      </select>
+      <br>
 
-      <button type="button" class="btn btn-outline-success" v-on:click.prevent="createMeal(selectedOption)">Create Meal</button>
+      <label>Flow ID: </label>
+      <input v-model="flowId" placeholder="Insert flow ID">
+      <br>
+
+      <label>Flow Name: </label>
+      <input v-model="flowName" placeholder="Insert flow name">
+      <br>
+
+      <label>Flow Table: </label>
+      <input v-model="flowTable" placeholder="Insert flow Table [0-255]">
+      <br>
+
+      <label>Priority: </label>
+      <input v-model="priority" placeholder="Priority">
+      <br><br>
+
+      <div>
+        <h4>Match</h4>
+        <label>In Port: </label>
+        <input v-model="port" placeholder="Port">
+        <br>
+        <label>VLAN ID: </label>
+        <input v-model="vlanId" placeholder="[0-4095]">
+        <br>
+        <label>IPv4 Source: </label>
+        <input v-model="ipSource" placeholder="<ip>/<mask>">
+        <br>
+        <label>IPv4 Destination: </label>
+        <input v-model="ipDest" placeholder="<ip>/<mask>">
+        <br>
+      </div>
+      <br>
+      <div>
+        <h4>Actions</h4>
+        <input type="checkbox" id="checkDrop" v-model="checkedDrop">
+        <label>Drop</label>
+      </div>
+
+      <button type="button" class="btn btn-outline-success" v-on:click.prevent='addFlow(device, flowTable, flowId)'>Add Flow</button>
+      <p>Teste Final: {{teste}}</p>
       <button type="button" class="btn btn-outline-danger" v-on:click.prevent="goBack">Cancel</button>
     </div>
   </div>
@@ -21,37 +62,50 @@
   module.exports = {
     data() {
       return {
-        tablesWithoutActiveMealsAtTheMoment: [],
-        selectedOption: null,
-        successMessage: "",
-        failMessage: "",
-        showSuccess: false,
-        showFailure: false
+        switchesInfo: [],
+        teste: [],
+        device: "",
+        flowTable: "",
+        flowId: "",
+        priority: "",
+        flowName: "",
+        port: "",
+        vlanId: "",
+        ipSource: "",
+        ipDest: "",
+        checkedDrop: "",
+
       };
     },
     methods: {
-      createMeal: function (table_number) {
-        axios.post("/api/meals/createMealOnTable/" + table_number + "/onWaiter/" + this.$store.getters.getAuthUser.id)
-          .then(response => {
-            this.successMessage = "Meal created successfully!";
-            this.showSuccess = true;
-            this.goBack();
-          })
-          .catch(function (error) {
-            console.log(error);
-            this.failMessage = "Meal not created!";
-            this.showFailure = true;
-          });
+      getSwitchesInfo: function () {
+        this.switchesInfo = [];
+          var vm = this;
+          axios.get('api/nodeSummary')
+            .then(function (response){
+              vm.switchesInfo = response.data;
+            })
+            .catch(function (error){
+              vm.switchesInfo = 'An error occurred.' + error;
+            });
       },
+
+      addFlow: function (device, flowTable, flowId) {
+        this.teste = [];
+          var vm = this;
+          axios.put('api/createFlow' + device + flowTable + flowId)
+            .then(response => {
+              this.getTablesS1();
+            })
+      },
+
       goBack() {
         this.$router.push('/flowsSummary');
       },
     },
     mounted() {
-      axios.get("/api/meals/tablesWithoutActiveMeals")
-        .then(response => {
-          this.tablesWithoutActiveMealsAtTheMoment = response.data;
-        });
+      this.getSwitchesInfo();
+      this.addFlow();
     }
   };
 </script>
